89748e200a8d0487d334221ee7ad4f4349319df3
Kaique Costa <kaiquenagato94@outlook.com>
2023-08-23 16:44:23 -0300

    more styles add

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@ category = ["chinese", "italian", "japanese", "french", "belgian"]
   Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
     category: category.sample
   )
 end
